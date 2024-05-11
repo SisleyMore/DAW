fc43fdb10ac889f310f0c5d83663868565a6b365
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2024-05-10 19:11:37 -0500

    Feat: add `add to cart` button and handle its functionality

--- src/app/shop/components/product-card/product-card.component.ts
+++ src/app/shop/components/product-card/product-card.component.ts
@@ -1,18 +1,20 @@
-import {Component, input} from '@angular/core';
-import {Product} from "../../../models/product.interface";
-import {CurrencyPipe} from "@angular/common";
-import {ButtonModule} from "primeng/button";
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject, input } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { ShoppingCartStore } from '../../+store/shopping-cart.store';
+import type { Product } from '../../../models/product.interface';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [
-    CurrencyPipe,
-    ButtonModule
-  ],
+  imports: [CurrencyPipe, ButtonModule],
   templateUrl: './product-card.component.html',
-  styles: ``
 })
 export class ProductCardComponent {
-  public readonly product= input.required<Product>();
+  public readonly product = input.required<Product>();
+  private readonly cartStore = inject(ShoppingCartStore);
+
+  public addToCart() {
+    this.cartStore.addToCart(this.product());
+  }
 }
