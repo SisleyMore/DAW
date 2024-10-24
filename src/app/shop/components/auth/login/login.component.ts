00c23a7fd8875f9a08ea3735f7a1a43d3b2db384
SisleyMore20 <sdrmy.20@gmail.com>
2024-10-23 21:04:25 -0500

    update

--- src/app/shop/components/auth/login/login.component.ts
+++ src/app/shop/components/auth/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // Asegúrate de importar FormsModule
+import { FormsModule } from '@angular/forms'; 
 import { User } from '../../../../models/user.interface';
 import { AuthService } from '../../../../service/auth.service';
-import { Router } from '@angular/router'; // Importar Router
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-login',
@@ -15,20 +15,19 @@ export class LoginComponent {
   user: User = { id: 0, email: '', fullName: '', password: '', createdAt: '', updatedAt: '' };
   email: string = '';
   password: string = '';
+  msgError: string = '';
 
-  constructor(private authService: AuthService, private router: Router) {} // Inyectar Router
+  constructor(private authService: AuthService, private router: Router) {} 
 
-  // Método para manejar el inicio de sesión
   onLogin() {
     this.authService.login(this.email, this.password).subscribe({
       next: (response: any) => {
         localStorage.setItem('token', response.token);
-        localStorage.setItem('userName', this.email); // Guarda el nombre o email del usuario en localStorage
-        this.router.navigate(['/home']); // Redirigir a la vista principal
+        localStorage.setItem('userName', this.email); 
+        this.router.navigate(['/home']); 
       },
       error: (err: any) => {
-        console.error('Error en el inicio de sesión:', err);
-        // Manejo de errores
+        this.msgError = 'El usuario o contraseña ingresado es incorrecto.'
       },
     });
   }
